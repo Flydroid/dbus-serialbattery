c356f28ba64f7b7ac5ea357c4b9fc3063804ed29
Jonathan Graesser <jonathan.graesser@novatronfusion.com>
2025-07-14 14:05:19 +0200

    add Csbi_Can BMS support  to dbus-serialbattery.py

--- dbus-serialbattery/dbus-serialbattery.py
+++ dbus-serialbattery/dbus-serialbattery.py
@@ -44,6 +44,7 @@ from bms.pace import Pace
 from bms.renogy import Renogy
 from bms.seplos import Seplos
 from bms.seplosv3 import Seplosv3
+from bms.csbi_can import Csbi_Can
 
 # add ext folder to sys.path
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), "ext"))
@@ -74,6 +75,7 @@ supported_bms_types = [
     {"bms": Renogy, "baud": 9600, "address": b"\xf7"},
     {"bms": Seplos, "baud": 19200, "address": b"\x00"},
     {"bms": Seplosv3, "baud": 19200},
+    {"bms": Csbi_Can, "baud": 500000},
 ]
 
 # enabled only if explicitly set in config under "BMS_TYPE"
